5231b85f2089f969ea7d084c13cecf9d755fa07c
PoomSmart <thatchapon.unp@outlook.com>
2019-06-09 19:47:26 +0700

    .

--- repo/depiction_gen.py
+++ repo/depiction_gen.py
@@ -201,6 +201,12 @@ tweaks = [
         ],
         "description": "<p>This is a compatibility library for runtime modification tweaks via Substitute and Cydia Substrate.</p>\
             <p>The motivation of this project is the lack of Substrate support on A12/arm64e for those using Chimera. As Substitute API usually works better on this environment, libSubstitrate will, if available, try to use Substitute API first. Otherwise, it will fall back to Cydia Substrate.</p>"
+    },
+    {
+        "file": "pad56",
+        "title": "Pad56",
+        "min_ios": "7.0",
+        "description": "<p>5x6 icon layout for iPad and iPad Pro. That's it.</p>"
     }
 ]
 
