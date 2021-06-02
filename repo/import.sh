935ddf0921794611125d74872a94351df5a99205
Amy While <26681721+elihwyma@users.noreply.github.com>
2021-06-02 03:04:01 +0100

    Add Support for ZSTD (#6)

--- repo/import.sh
+++ repo/import.sh
@@ -6,3 +6,4 @@ dpkg-scanpackages -m ./debs /dev/null > Packages
 gzip -cf Packages > Packages.gz
 xz -9fkev Packages > Packages.xz
 bzip2 -cf Packages > Packages.bz2
+zstd -c19 Packages > Packages.zst
