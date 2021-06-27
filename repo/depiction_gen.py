0d4f240d2d2fd3a9db2c6f6ca8070102fa12746e
PoomSmart <thatchapon.unp@outlook.com>
2021-06-27 15:19:43 +0700

    -

--- repo/depiction_gen.py
+++ repo/depiction_gen.py
@@ -278,6 +278,7 @@ tweaks = [
             <p>If you encounter \"No stream Tap to Retry\" error for your video, you are recommended to upgrade YouTube to the latest version.</p>\
             <p>The changelogs are annotated with the versions of YouTube app that YouPiP supports.</p>",
         "changes": [
+            [ "1.4.0", "Attempted to fix PiP playback speedup bug using iOS 15.0b2 approach (15.22.4+)" ],
             [ "1.3.4", "Enable native PiP toggle inside YouTube settings (15.22.4+)" ],
             [ "1.3.3", "Refactored logic related to bootstraping PiP functionality (15.22.4+)" ],
             [ "1.3.2", "Fixed crash due to adding PiP button to the overlay for some users (15.22.4+)" ],
