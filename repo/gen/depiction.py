929e514b510acfdee3c416760cce08e8d038ec93
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2024-02-22 09:26:31 +0700

    lt

--- repo/gen/depiction.py
+++ repo/gen/depiction.py
@@ -261,8 +261,11 @@ tweaks = [
         "file": "latesttranslate",
         "title": "LatestTranslate",
         "min_ios": "14.0",
-        "max_ios": "15.8.1",
-        "description": "<p>Make Apple's Translate app support all languages to date. For example, make all iOS 16 languages available to iOS 15. If the tweak doesn't work, restart Translate app and ensure you have internet connection. If to no avail, you may reinstall this tweak.</p>"
+        "max_ios": "16.7.5",
+        "description": "<p>Make Apple's Translate app support all languages to date. For example, making all iOS 17 languages available to iOS 15 and 16. If the tweak doesn't work, restart Translate app and ensure you have internet connection. If to no avail, you may reinstall this tweak.</p>",
+        "changes": [
+            ["6.0.0", "Added new languages from iOS 17"]
+        ]
     },
 ] + youtube + emoji + camera
 
