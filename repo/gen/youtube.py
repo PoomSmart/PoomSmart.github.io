587f40c93f713081d1b224f4d7f08fda0f85d1cb
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2025-01-12 13:57:31 +0700

    yp

--- repo/gen/youtube.py
+++ repo/gen/youtube.py
@@ -58,6 +58,11 @@ youtube = [
         "description": "<p>Enable native PiP in iOS YouTube app.</p>\
             <p>YouPiP best supports the latest version of YouTube. You may downgrade to as far as version 16.29.4, older versions will not be (fully) supported.</p>",
         "changes": [
+            ["1.12.0", [
+                "Simplified PiP enabling logic",
+                "Fixed setting icon not appearing when the tweak is disabled and grouped settings experiment is on",
+                "When iOS Text Size is not default, the Video Tab Bar PiP button will be resized accordingly"
+            ]],
             ["1.11.0", "Added a new setting to make it possible for PiP to activate from either the video overlay button, the video tab bar button or dismissing the app at the same time"],
             ["1.10.1", "Possibly Fixed the tweak not working when PiP setting has never been on before"],
             ["1.10.0", "Updated for YTVideoOverlay 2.0.0"],
