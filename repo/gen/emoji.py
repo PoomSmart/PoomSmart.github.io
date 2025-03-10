95d21e770b1716f6a64b9cbf1fedd55f435f792e
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2025-03-10 17:16:29 +0700

    emjl

--- repo/gen/emoji.py
+++ repo/gen/emoji.py
@@ -124,6 +124,7 @@ emoji = [
         "strict_range": True,
         "no_sileo": True,
         "changes": [
+            ["1.8.7", "Fixed the tweak not working on iOS 7 - 8, 32-bit devices"],
             ["1.8.6", "Removed unused code for each architecture"],
             ["1.8.5", "Compiled with ARC"],
             ["1.8.4", "Fixed crash on iOS 6 due to using modern-only libroot"],
