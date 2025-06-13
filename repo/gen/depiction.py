b242c6f011720572fe2140ffd5f6f62e3278c0b5
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2025-06-13 18:22:45 +0700

    plf

--- repo/gen/depiction.py
+++ repo/gen/depiction.py
@@ -215,6 +215,10 @@ tweaks = [
         "description": "<p>Provides polyfills for some JavaScript features that are not available on old iOS versions.</p>\
             <p>Up until 2025, iOS 15 and lower are considered \"old\". A lot of websites would outright stop working on these versions as they no longer provide necessary polyfills.</p>",
         "changes": [
+            ["1.6.1", [
+                "Downgraded spoofed iOS version to 16.0 to improve compatibility on old devices",
+                "Restricted certain polyfills to execute only on the maximum unsupported iOS version and lower"
+            ]],
             ["1.6.0", [
                 "Fixed broken polyfill for RegExp lookbehind feature",
                 "Fixed spoofing user agent not working on non-Safari browsers",
