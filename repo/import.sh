f64f80518ef0249c8b39f315e7b686bc89e4800d
PoomSmart <3608783+PoomSmart@users.noreply.github.com>
2026-04-19 09:39:15 +0700

    Normalize Packages.gz

--- repo/import.sh
+++ repo/import.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 # rm -f depictions/* sileodepictions/*
 uv run python main.py
 dpkg-scanpackages -m ./debs /dev/null > Packages
-gzip -cf Packages > Packages.gz
+gzip -ncf Packages > Packages.gz
 xz -9fkev Packages > Packages.xz
 bzip2 -cf Packages > Packages.bz2
 zstd -c19 Packages > Packages.zst
