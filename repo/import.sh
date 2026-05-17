c2953f42dbacfc5fb0a2be7f12a7952e7f4df779
PoomSmart <3608783+PoomSmart@users.noreply.github.com>
2026-05-17 16:07:49 +0700

    ym

--- repo/import.sh
+++ repo/import.sh
@@ -5,8 +5,11 @@ set -euo pipefail
 # brew install dpkg zstd lz4
 
 # rm -f depictions/* sileodepictions/*
+echo "Importing depictions..."
 uv run python main.py
+echo "Generating Packages files..."
 dpkg-scanpackages -m ./debs /dev/null > Packages
+echo "Compressing Packages files..."
 gzip -ncf Packages > Packages.gz
 xz -9fkev Packages > Packages.xz
 bzip2 -cf Packages > Packages.bz2
